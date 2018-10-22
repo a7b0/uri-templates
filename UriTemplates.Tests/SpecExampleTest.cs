a38ab617c05999b1b07fa4e77085a924e9018ce0
Espen Wang Andreassen <espen.wang.andreassen@nrk.no>
2018-10-22 17:23:19 +0200

    Fixed visibility on test methods Added NUnit Test Adapter

--- UriTemplates.Tests/SpecExampleTest.cs
+++ UriTemplates.Tests/SpecExampleTest.cs
@@ -3,19 +3,18 @@
 namespace Resta.UriTemplates.Tests
 {
     [TestFixture]
-    
     public class SpecExampleTest
     {
         private const string FileName = "testcases/spec-examples.json";
 
         [Test, TestCaseSource(typeof(TestCaseData), nameof(TestCaseData.GetSamples), new object[] { FileName })]
-        protected void SpecTest(TestCase testCase)
+        public void SpecTest(TestCase testCase)
         {
             SpecBaseTests.SpecTest(testCase);
         }
 
         [Test, TestCaseSource(typeof(TestCaseData), nameof(TestCaseData.GetInvalidSamples), new object[] { FileName })]
-        protected void SpecInvalidTest(TestCase testCase)
+        public void SpecInvalidTest(TestCase testCase)
         {
             SpecBaseTests.SpecInvalidTest(testCase);
         }
--- UriTemplates.Tests/SpecNegativeTests.cs
+++ UriTemplates.Tests/SpecNegativeTests.cs
@@ -9,13 +9,13 @@ namespace Resta.UriTemplates.Tests
         private const string FileName = "testcases/negative-tests.json";
 
         [Test, TestCaseSource(typeof(TestCaseData), nameof(TestCaseData.GetSamples), new object[] { FileName })]
-        protected void SpecTest(TestCase testCase)
+        public void SpecTest(TestCase testCase)
         {
             SpecBaseTests.SpecTest(testCase);
         }
 
         [Test, TestCaseSource(typeof(TestCaseData), nameof(TestCaseData.GetInvalidSamples), new object[] { FileName })]
-        protected void SpecInvalidTest(TestCase testCase)
+        public void SpecInvalidTest(TestCase testCase)
         {
             SpecBaseTests.SpecInvalidTest(testCase);
         }
